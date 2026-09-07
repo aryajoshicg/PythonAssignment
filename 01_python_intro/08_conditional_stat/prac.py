cb98d1a113ecbd47eb9e4f4ee407b1cea260cf2e
aryajoshicg <arya.joshi.cg@gmail.com>
2026-09-07 11:27:27 +0530

    message

---
+++ 01_python_intro/08_conditional_stat/prac.py
@@ -0,0 +1,19 @@
+is_indian = input("Are you Indian? Yes or No:")
+
+# if is_indian=="yes":
+#     age = int(input("Enter your age."))
+#     if age>=18:
+#         print("You are allowed to have a driving license")
+#     if age>=21:
+#         print("You are allowed to adopt a child")
+# if is_indian=="no":
+#     print("You are not allowed to vote")
+if is_indian=="yes":
+    print("Congratulations, you are selected!!!")
+else :
+    que = input("Have you ever resided in India? yes or no")
+    if que=="yes" :
+        print("You are selected!!")
+    if que=="no" :
+        print("You are not selected!!")    
+
