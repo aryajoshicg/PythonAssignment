8d721d829be17467a186b855d0365b39d16e3f6f
aryajoshicg <arya.joshi.cg@gmail.com>
2026-09-08 23:06:49 +0530

    new

---
+++ 01_python_intro/04_arith_operations/T18.py
@@ -0,0 +1,11 @@
+value = int(None)
+a = 27
+print("value" + a)
+print("value" - a)
+print("value" * a)
+print("value" / a)
+print("value" // a)
+print("value" % a)
+print("value" ** a)
+
+#type errors over concatenation
--- 01_python_intro/practice.py
+++ 01_python_intro/practice.py
@@ -10,8 +10,16 @@
 #print("abc"<"ab")
 # print(" " and None)
 # print(" " or None)
-print("operation \n  Addition \n Subtraction \n Multiplication \n Division")
-print(input("Enter your operation"))
-number1 = int(input("Enter number 1:"))
-number2 = int(input("Enter number 2:"))
-if number == ("")
+# print("operation \n  Addition \n Subtraction \n Multiplication \n Division")
+# print(input("Enter your operation"))
+# number1 = int(input("Enter number 1:"))
+# number2 = int(input("Enter number 2:"))
+# if number == ("")
+a = 232
+x = a // 10
+y = a % 10
+z = x - 18
+print(x)
+print(y)
+print(z)
+print(y + z)
