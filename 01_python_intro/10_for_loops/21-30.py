5222c1e134345c88b12b52ebad6feffe00521687
aryajoshicg <arya.joshi.cg@gmail.com>
2026-09-21 12:25:32 +0530

    changes in problems

--- 01_python_intro/10_for_loops/21-30.py
+++ 01_python_intro/10_for_loops/21-30.py
@@ -4,16 +4,84 @@
 #     print(ch)
 
 
+
 # name = input("Enter a word: ")
 
 # for ch in name:
 #     print(ch, end=" ")
 
 
-name = input("Enter a word: ")
 
-count = 0
+# name = input("Enter a word: ")
 
-for ch in name:
-    count +=1
-    print(ch)
+# count = 0
+
+# for ch in name:
+#     count=count+1
+# print("The count is:", count)
+
+
+# name = input("Enter a word:")
+
+# count = 0
+
+# for character in name:
+#     if character == "a":
+#         count = count + 1
+
+# print("Count:", count)
+
+
+
+
+
+
+
+# name = input("Enter a word:")
+
+# for character in name:
+
+
+
+
+
+
+# for i in range(3):
+#     for j in range(4):
+#         print("*", end=" ")
+#     print()     
+
+
+
+
+
+# for i in range(4):
+#     for j in range(4):
+#         print("*", end=" ")
+#     print() 
+
+
+
+
+
+
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()   
+
+
+for i in range(1,6):
+    print("*" * i)
+
+
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j, end="")
+#     print()   
+
+
+
+  
