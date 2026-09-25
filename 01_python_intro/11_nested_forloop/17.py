be3587a8b747fc71de6e4500d9ad04d15b0dceb0
aryajoshicg <arya.joshi.cg@gmail.com>
2026-09-25 12:04:05 +0530

    Nested For Loop Practice

---
+++ 01_python_intro/11_nested_forloop/17.py
@@ -0,0 +1,6 @@
+a = 1
+for i in range(3):
+    for j in range(3):
+        print(a, end=" ")
+        a +=1
+    print()
---
+++ 01_python_intro/11_nested_forloop/18.py
@@ -0,0 +1,6 @@
+a = 1
+for i in range(4):
+    for j in range(5):
+        print(a, end=" ")
+        a +=1
+    print()
